92af54da00d60d6c5ef0d89c276192f4074fb05b
Josue Mendez <josuejoelmm@gmail.com>
2020-12-17 14:40:30 -0300

    set full height

--- src/app/pages/Home/HomeContainer.js
+++ src/app/pages/Home/HomeContainer.js
@@ -5,7 +5,10 @@ import usePeopleState from '../../hooks/usePeopleState';
 
 const styles = {
     padding: '4em 2em',
-    background: 'lightgrey'
+    background: 'lightgrey',
+    display: 'flex',
+    flex: '1 1',
+    flexDirection: 'column'
 }
 
 export default function HomeContainer() {
